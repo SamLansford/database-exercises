d511395354dce50521b0655572c1c50cdd3b2545
Sam Lansford <sam.j.lansford@gmail.com>
2021-04-05 14:58:00 -0500

    doing join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,12 +1,12 @@
 Use employees;
 
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
-FROM employees as e
-JOIN dept_emp as de
-ON de.emp_no = e.emp_no
-JOIN departments as d
-ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+# SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+# FROM employees as e
+# JOIN dept_emp as de
+# ON de.emp_no = e.emp_no
+# JOIN departments as d
+# ON d.dept_no = de.dept_no
+# WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
 describe employees;
 
@@ -17,7 +17,7 @@ join dept_manager as dm
 on dm.emp_no = e.emp_no
 join departments as d
 on d.dept_no = dm.dept_no
-where dm.to_date = '9999-01-01'
+where dm.to_date > now()
 order by dept_name;
 
 
@@ -27,7 +27,35 @@ join dept_manager as dm
 on dm.emp_no = e.emp_no
 join departments as d
 on d.dept_no = dm.dept_no
-where dm.to_date = '9999-01-01'
+where dm.to_date > now()
 and gender = 'F'
 order by dept_name;
+
+
+
+select t.title as Title, count(t.emp_no) as Total
+from titles as t
+join dept_emp as dm
+on dm.emp_no = t.emp_no
+join departments as d
+on dm.dept_no = d.dept_no
+where dm.to_date > now()
+and d.dept_name = 'Customer Service'
+and t.to_date > now()
+group by title;
+
+
+select d.dept_name, concat(e.first_name, ' ', e.last_name) as Full_Name, s.salary as Salary
+from employees as e
+join dept_manager as dm
+on dm.emp_no = e.emp_no
+join departments as d
+on d.dept_no = dm.dept_no
+join salaries as s
+on e.emp_no = s.emp_no
+where dm.to_date > now()
+and s.to_date > now()
+order by dept_name;
+
+
 
